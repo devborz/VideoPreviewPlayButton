fefaad6a7b3dac693ad6e2d63ad12b9e21d3f804
Usman Turkaev <u.turkaev@yandex.ru>
2022-01-31 17:46:29 +0300

    Update ViewController.swift

--- VideoPreviewPlayButton/ViewController.swift
+++ VideoPreviewPlayButton/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoButton: VideoPreviewPlayButton!
+    
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        videoButton.delegate = self
+//        videoButton.currentState = .play
     }
-
 
 }
 
+extension ViewController: VideoPreviewPlayButtonDelegate {
+    func didStartDownloading(_ button: VideoPreviewPlayButton) {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { [weak self] _ in
+            self?.videoButton.downloadProgress += 0.1
+        })
+    }
+    
+    func didCancelDownloading(_ button: VideoPreviewPlayButton) {
+        timer?.invalidate()
+    }
+    
+    func didTapPlayButton(_ button: VideoPreviewPlayButton) {
+        print("Play button tapped")
+    }
+}
